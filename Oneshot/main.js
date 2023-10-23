a694b61d2ee8943291a7110c06dd73912c9d9a9c
DarkHost <sultanisk@mail.ru>
2023-10-23 18:16:36 +0600

    Fixes
    1

--- Oneshot/main.js
+++ Oneshot/main.js
@@ -1,24 +1,22 @@
 //Timer functionality
 function timer() {
     var currentTime = new Date();
-    const targetTime = new Date('2023-12-25T23:59:59');
+    const targetTime = new Date();
     var remainingTime = targetTime - currentTime;
-
+    
     if (remainingTime <= 0) {
         if (sessionStorage.getItem('alerted') != 'false'){
             alert("Game Update has been released!");
             sessionStorage.setItem('alerted', 'false');
         }
-        
         document.getElementById('countdown').innerHTML = 'Update is released. Check the new version of the Game!'
-    }
-    else{
+        
+    } else{
         var second = Math.floor((remainingTime / 1000) % 60);
         var minute = Math.floor((remainingTime / 1000 / 60) % 60);
         var hour = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
-        var day =  Math.floor(remainingTime / (1000 * 60 * 60 * 24));
 
-        document.getElementById('countdown').innerHTML = day + " days " + hour + " hours " + minute + " minutes " + second + " seconds left until the update!";
+        document.getElementById('countdown').innerHTML = hour + " hours " + minute + " minutes " + second + " seconds left until the update!";
     }
 }
 
