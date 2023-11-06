7a9fc75600d22f0cb8aaadad9c183ffed12657fc
Nurkhat <113333558+nktwn@users.noreply.github.com>
2023-11-06 17:45:44 +0600

    new

--- Oneshot/adminPage.html.js
+++ Oneshot/adminPage.html.js
@@ -1,16 +1,70 @@
-
+// Функция для отображения пользователей и кнопок управления
 function displayUsers() {
-    const usersList = document.getElementById('usersList');
-    usersList.innerHTML = '';
-    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const usersListElement = document.getElementById('usersList');
+    usersListElement.innerHTML = '';
 
-    users.forEach(user => {
-        const userElement = document.createElement('li');
-        userElement.className = 'list-group-item';
-        userElement.textContent = `Email: ${user.email}, Password: ${user.password}`;
-        usersList.appendChild(userElement);
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.forEach((user, index) => {
+        // Создаем элемент списка для пользователя
+        const userItem = document.createElement('li');
+        userItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+        userItem.textContent = user.email;
+
+        // Создаем кнопку Delete
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.classList.add('btn', 'btn-danger', 'btn-sm');
+        deleteBtn.onclick = function() { deleteUser(index); };
+        userItem.appendChild(deleteBtn);
+
+        // Создаем кнопку Change
+        const changeBtn = document.createElement('button');
+        changeBtn.textContent = 'Change';
+        changeBtn.classList.add('btn', 'btn-primary', 'btn-sm', 'mx-2');
+        changeBtn.onclick = function() { showChangePasswordForm(user.email, index); };
+        userItem.appendChild(changeBtn);
+
+        // Добавляем элемент списка на страницу
+        usersListElement.appendChild(userItem);
     });
 }
 
 
-window.onload = displayUsers;
+// Функция для удаления пользователя
+function deleteUser(index) {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    users.splice(index, 1);
+    localStorage.setItem('users', JSON.stringify(users));
+    displayUsers();
+}
+
+// Функция для отображения формы изменения пароля
+function showChangePasswordForm(email, index) {
+    // Показываем форму и заполняем информацию о пользователе
+    document.getElementById('passwordChangeCard').style.display = 'block';
+    document.getElementById('userEmail').textContent = `Change password for ${email}`;
+    document.getElementById('userIndex').value = index;
+}
+
+// Обработка формы изменения пароля
+document.getElementById('passwordChangeForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем стандартное поведение формы
+
+    const index = document.getElementById('userIndex').value;
+    const newPassword = document.getElementById('newPasswordInput').value;
+    changeUserPassword(index, newPassword);
+});
+
+// Функция изменения пароля пользователя
+function changeUserPassword(index, newPassword) {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    // Здесь должна быть проверка на безопасность пароля, но для примера пропускаем
+    users[index].password = newPassword; // Изменяем пароль
+    localStorage.setItem('users', JSON.stringify(users)); // Обновляем localStorage
+    displayUsers(); // Обновляем список пользователей
+    alert('Password changed successfully.'); // Уведомляем об успешном изменении
+    document.getElementById('passwordChangeCard').style.display = 'none'; // Скрываем форму
+}
+
+// При загрузке страницы отображаем пользователей
+document.addEventListener('DOMContentLoaded', displayUsers);
